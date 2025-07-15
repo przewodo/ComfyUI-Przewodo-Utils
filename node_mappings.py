9839c4dd8a127b2c73a0061ea158fa660656293b
Rodrigo Moré Przewodowski <rodrigo@sevenpercent.com.br>
2025-07-15 04:04:02 -0300

    First implementation of wan_first_last_first_frame_to_video.py

--- node_mappings.py
+++ node_mappings.py
@@ -2,9 +2,11 @@ try:
     from .compare_numbers_to_combo import *
     from .image_scale_factor import *
     from .image_sizer_node import *
-    from .wan_get_max_image_resolution_by_aspect_ratio import *
     from .swap_any_condition import *
     from .swap_any_comparison import *
+    from .swap_image_comparison import *
+    from .wan_get_max_image_resolution_by_aspect_ratio import *
+    from .wan_first_last_first_frame_to_video import *
     
 except ImportError:
     print("\033[Przewodo Utils: \033[92mFailed to load Essential nodes\033[0m")
@@ -16,7 +18,9 @@ NODE_CLASS_MAPPINGS = {
     "przewodo ImageScaleFactor": ImageScaleFactor,
     "przewodo ImageSizer": ImageSizer,
     "przewodo SwapAnyCondition": SwapAnyCondition,
-    "przewodo SwapAnyComparison": SwapAnyComparison
+    "przewodo SwapAnyComparison": SwapAnyComparison,
+    "przewodo SwapImageComparison": SwapImageComparison,
+    "przewodo WanFirstLastFrameToVideo": WanFirstLastFrameToVideo,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -25,5 +29,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "przewodo ImageScaleFactor": "Image Scale Factor",    
     "przewodo ImageSizer": "Image Sizer",
     "przewodo SwapAnyCondition": "Swap any Two values in a condition",
-    "przewodo SwapAnyComparison": "Swap any Two values in a comparison"    
+    "przewodo SwapAnyComparison": "Swap any Two values in a comparison",
+    "przewodo SwapImageComparison": "Swap Two Images in a comparison",
+    "przewodo WanFirstLastFrameToVideo": "WanFirstLastFrameToVideo",
 }
---
+++ swap_image_comparison.py
@@ -0,0 +1,49 @@
+import math
+import sys
+import os
+from .core import COMPARE_FUNCTIONS, any
+
+class SwapImageComparison:
+    @classmethod
+    def IS_CHANGED(cls, comparison_type=None, **kwargs):
+        # Always trigger recalculation/redraw when comparison_type changes
+        return True
+
+    @classmethod
+    def INPUT_TYPES(cls, **kwargs):
+
+        compare_functions = list(COMPARE_FUNCTIONS.keys())
+
+        optional = {
+            "input_a": ("IMAGE"),
+            "input_b": ("IMAGE"),
+        }
+
+        required = {
+            "comparison": (compare_functions, {"default": "a == b"}),
+            "value_a": (any),
+            "value_b": (any),
+        }
+
+        return {"optional": optional, "required": required}
+
+    RETURN_TYPES = (("IMAGE"), ("IMAGE"),)
+    RETURN_NAMES = ("input_a", "input_b")
+    FUNCTION = "run"
+    CATEGORY = "PrzewodoUtils"
+
+    def run(
+        self,
+        input_a=None,
+        input_b=None,
+        comparison=None,
+        value_a=None,
+        value_b=None,
+        **kwargs
+    ):
+        if COMPARE_FUNCTIONS[comparison](value_a, value_b):
+            return (input_b, input_a,)
+        else:
+            return (input_a, input_b)
+        
+        return (input_a, input_b)
