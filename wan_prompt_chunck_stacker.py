629f2b7b3eb0016b54bcfea7d46f393e58582dbd
Rodrigo Moré Przewodowski <rodrigo@sevenpercent.com.br>
2025-08-13 14:13:04 -0300

    Refactor input parameter for chunk index in WanPromptChunkStacker to start_on_seconds

--- wan_prompt_chunck_stacker.py
+++ wan_prompt_chunck_stacker.py
@@ -10,7 +10,7 @@ class WanPromptChunkStacker:
                 "lora_stack": (any_type, {"default": None, "advanced": True, "tooltip": "Stack of LoRAs to apply to the diffusion model. Each LoRA modifies the model's behavior."}),
                 "positive_prompt": ("STRING", {"default": "", "multiline": True, "tooltip": "Positive prompt text for this chunk. Describes what you want to generate in the video segment"}),
                 "negative_prompt": ("STRING", {"default": "", "multiline": True, "tooltip": "Negative prompt text for this chunk. Describes what you want to avoid in the video segment"}),
-                "chunk_index_start": ("INT", {"default": 0, "min": 0, "max": 1000, "step": 1, "tooltip": "The starting index for this chunk video to be used."}),
+                "start_on_seconds": ("INT", {"default": 0, "min": 0, "max": 1000, "step": 1, "tooltip": "The starting seconds (chunk) of the video."}),
             },
         }
 
@@ -20,10 +20,12 @@ class WanPromptChunkStacker:
 
     CATEGORY = "PrzewodoUtils/Wan"
 
-    def run(self, previous_prompt=None, lora_stack=None, positive_prompt="", negative_prompt="", chunk_index_start=0):
+    def run(self, previous_prompt=None, lora_stack=None, positive_prompt="", negative_prompt="", start_on_seconds=0):
         if (previous_prompt is None):
             previous_prompt = []
 
-        previous_prompt.append([positive_prompt, negative_prompt, chunk_index_start, lora_stack])
+        chunk_index = start_on_seconds // 5 if start_on_seconds > 0 else 0
+
+        previous_prompt.append([positive_prompt, negative_prompt, chunk_index, lora_stack])
 
         return (previous_prompt,)
